9904eb5b9631407c131f63294dcd691aa44da5a6
Alexandre10y <alexandre10y@outlook.com>
2024-02-27 22:30:11 -0300

    upload list to 1 step

---
+++ todo/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import Todo from './components/todo';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar Funcionabilidaded X no Sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir a Academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudo",
+      isCompleted: false,
+    },
+  ])
+
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <Todo todo={todo}/>
+      ))}
+    </div>
+  </div>
+}
+
+export default App;
