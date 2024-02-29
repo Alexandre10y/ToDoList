dd270a1ba74b0c6a68e9bed9396726e3079830c9
Alexandre10y <alexandre10y@outlook.com>
2024-02-29 10:50:26 -0300

    atualizando tarefas
    carregando tarefas, verificando vazio, finalizando estilização

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -23,14 +23,24 @@ function App() {
     },
   ])
 
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 1000),
+      text,
+      category,
+      isCompleted: false,
+      },
+    ];
+    setTodos(newTodos)
+  }
   return <div className="app">
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {todos.map((todo) => (
-        <Todo todo={todo}/>
+        <Todo key={todo.id} todo={todo} />
       ))}
     </div>
-        <TodoForm/>
+    <TodoForm addTodo={addTodo}/>
   </div>
 }
 
